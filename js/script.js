ac88b001cc5e3d175f8f9f832588c6d173d6fb1e
viet_v <vohoangviet459@gmail.com>
2021-10-26 23:06:40 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var button = document.querySelector(".got-this");
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+
+button.addEventListener("click", function () {
+  openModal();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    if (!modal.classList.contains("show-modal")) {
+      openModal();
+    }
+  }
+});
+modalX.addEventListener("click", function () {
+  closeModal();
+});
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
+    }
+  }
+});
+
+var closeModal = function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
+var openModal = function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+};
